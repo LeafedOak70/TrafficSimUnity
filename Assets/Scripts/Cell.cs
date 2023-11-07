5f5225e094da78b686b377f7e9809893e8efc2c5
Dulguun Enkhtsog <dulguun0614@gmail.com>
2023-11-07 10:42:34 -0500

    Initial Commit

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public bool collapsed;
+    public int x;
+    public int y;
+    public Tile[] tileOptions;
+    public int index;
+
+    public void CreateCell(bool collapseState, int x, int y, Tile[] tiles, int index)
+    {
+        collapsed = collapseState;
+        this.x = x;
+        this.y = y;
+        tileOptions = tiles;
+        this.index = index;
+    }
+    public void setTiles(Tile[] tiles)
+    {
+        tileOptions = tiles;
+    }
+    
+
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour{
+    public enum TerrainType { None, SPlus, SMinus, Road, Building };
+    public List<Tile> upNeighbours;
+    public List<Tile> downNeighbours;
+    public List<Tile> leftNeighbours;
+    public List<Tile> rightNeighbours;
+
+
+    public TerrainType north;
+    public TerrainType south;
+    public TerrainType east;
+    public TerrainType west;
+}
