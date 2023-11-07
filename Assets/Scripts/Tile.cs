2d606723d04040411793cf0fed9082870c8238f2
Dulguun Enkhtsog <dulguun0614@gmail.com>
2023-11-07 11:07:04 -0500

    Updated prefabs

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tile : MonoBehaviour{
-    public enum TerrainType { None, SPlus, SMinus, Road, Building };
+    public enum TerrainType { None, Grass, Road, Building };
     public List<Tile> upNeighbours;
     public List<Tile> downNeighbours;
     public List<Tile> leftNeighbours;
