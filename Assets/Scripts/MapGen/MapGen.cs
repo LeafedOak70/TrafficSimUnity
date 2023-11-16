8ed703930ed0738377f2aa7edf0ed677b7a22c8f
Dulguun Enkhtsog <dulguun0614@gmail.com>
2023-11-16 00:35:08 -0500

    MOER PERLIN LUL
    Perlin map gen~

--- Assets/Scripts/MapGen/MapGen.cs
+++ Assets/Scripts/MapGen/MapGen.cs
@@ -10,19 +10,19 @@ public class MapGen : MonoBehaviour
 
     private void Update()
     {
-        float[,] perlinMap = perlinNoiseGenerator.GeneratePerlinMap();
+        float[,] perlinMap = perlinClass.getPerlinMap();
         GenerateTiles(perlinMap);
     }
 
     void GenerateTiles(float[,] perlinMap)
     {
-        for (int x = 0; x < width; x++)
+        for (int x = 0; x < perlinClass.width; x++)
         {
-            for (int y = 0; y < height; y++)
+            for (int y = 0; y < perlinClass.height; y++)
             {
                 float sample = perlinMap[x, y];
                 GameObject tilePrefab = ChooseTilePrefab(sample);
-                Instantiate(tilePrefab, new Vector3(x, 0, y), Quaternion.identity);
+                Instantiate(tilePrefab, new Vector3(x, y, 0), Quaternion.identity);
             }
         }
     }
